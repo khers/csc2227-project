82dca14081d8155f8e9a833d85d671e888e3d21f
Eric B Munson <emunson@mgebm.net>
2020-04-10 12:12:04 -0400

    Implement normal packet reception
    Instead of using NF hooks and trying to do all the work in the interrupt
    context, lets see how the "normal" way of doing things turns out.
    
    Signed-off-by: Eric B Munson <emunson@mgebm.net>

--- module/kern-store.c
+++ module/kern-store.c
@@ -3,47 +3,58 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
-#include <linux/netfilter.h>
-#include <linux/netfilter_ipv4.h>
 #include <linux/skbuff.h>
 #include <linux/udp.h>
 #include <linux/socket.h>
 #include <linux/uio.h>
 #include <linux/ip.h>
 #include <linux/inet.h>
+#include <linux/hashtable.h>
+#include <linux/kthread.h>
 #include <asm/string.h>
 
 #ifndef CHAIN_PORT
 #define CHAIN_PORT 1345
 #endif
 
-static void respond(u32 addr)
-{
-	struct sockaddr_in send;
-	struct socket *socket;
-	struct msghdr msg;
+#define MAX_MSG 0xFFFF
+
+struct kern_store {
+	struct socket *sock;
+	struct task_struct *thread;
+	int running;
+};
+
+static struct kern_store *server_state;
+
+struct incoming {
+	struct msghdr hdr;
+	struct sockaddr_in addr;
 	struct kvec vec;
+};
+
+static void respond(struct incoming *msg)
+{
+	struct socket *socket;
+	struct msghdr hdr;
+	struct kvec vec;
+	mm_segment_t old_fs;
 	int ret;
 
 	if (sock_create_kern(&init_net, AF_INET, SOCK_DGRAM, IPPROTO_UDP, &socket))
 		return;
 
-	memset(&send, 0, sizeof(struct sockaddr_in));
-	send.sin_family = AF_INET;
-	send.sin_port = CHAIN_PORT;
-	send.sin_addr.s_addr = addr;
-
-	if (socket->ops->connect(socket, (struct sockaddr*)&send, sizeof(struct sockaddr_in), 0)) {
+	if (socket->ops->connect(socket, (struct sockaddr*)&(msg->addr), sizeof(struct sockaddr_in), 0)) {
 		printk(KERN_WARNING "Failed to connect to receiving host\n");
 		goto out_release;
 	}
 
-	memset(&msg, 0, sizeof(struct msghdr));
-	msg.msg_name = &send;
-	msg.msg_namelen = sizeof(struct sockaddr_in);
-	msg.msg_iter.type = ITER_KVEC;
-	msg.msg_iter.count = 1;
-	msg.msg_iter.nr_segs = 1;
+	memset(&hdr, 0, sizeof(struct msghdr));
+	hdr.msg_name = &(msg->addr);
+	hdr.msg_namelen = sizeof(struct sockaddr_in);
+	hdr.msg_iter.type = ITER_KVEC;
+	hdr.msg_iter.count = 1;
+	hdr.msg_iter.nr_segs = 1;
 	vec.iov_base = kzalloc(9, GFP_NOWAIT);
 	if (!vec.iov_base) {
 		printk(KERN_WARNING "Failed to allocate memory\n");
@@ -51,8 +62,11 @@ static void respond(u32 addr)
 	}
 	strcpy(vec.iov_base,"response");
 	vec.iov_len = 9;
-	msg.msg_iter.kvec = &vec;
-	ret = sock_sendmsg(socket, &msg);
+	hdr.msg_iter.kvec = &vec;
+	old_fs = get_fs();
+	set_fs(KERNEL_DS);
+	ret = sock_sendmsg(socket, &hdr);
+	set_fs(old_fs);
 	if (ret < 0) {
 		printk(KERN_WARNING "Failed to send\n");
 		goto out_free;
@@ -62,67 +76,130 @@ out_free:
 	kfree(vec.iov_base);
 out_release:
 	sock_release(socket);
+	kfree(msg->vec.iov_base);
+	kfree(msg);
 }
 
-#ifdef USE_NETHOOK
-static unsigned int packet_interceptor(void *priv, struct sk_buff *skb,
-				       const struct nf_hook_state *state)
+static int run_server(void *ignored)
 {
-	struct iphdr *ip;
-	struct udphdr *udp;
+	struct incoming *msg;
+	int size, ret = 0;
+	mm_segment_t old_fs;
 
-	if (!skb)
-		return NF_DROP;
+	current->flags |= PF_NOFREEZE;
 
-	ip = (struct iphdr *)skb_network_header(skb);
-	if (!ip)
-		return NF_DROP;
-
-	if (ip->protocol == IPPROTO_UDP) {
-		udp = (struct udphdr *)skb_transport_header(skb);
-		if (!udp)
-			return NF_DROP;
-
-		if (ntohs(udp->dest) == CHAIN_PORT) {
-			/* Handle incoming request */
-			respond(ntohl(ip->saddr));
-			return NF_DROP;
+	for (;;) {
+		msg = kzalloc(sizeof(struct incoming), 0);
+		if (!msg) {
+			printk(KERN_ERR "Failed to allocate memory for incoming message, ternimating kvvd\n");
+			ret = -ENOMEM;
+			goto out;
 		}
+
+		msg->vec.iov_len = MAX_MSG;
+		msg->vec.iov_base = kzalloc(MAX_MSG, 0);
+		if (!msg->vec.iov_base) {
+			printk(KERN_ERR "Failed to allocate memory for incoming buffer, ternimating kvvd\n");
+			ret = -ENOMEM;
+			goto out_msg;
+		}
+
+		msg->hdr.msg_name = &msg->addr;
+		msg->hdr.msg_namelen = sizeof(struct sockaddr_in);
+		msg->hdr.msg_iter.type = ITER_KVEC;
+		msg->hdr.msg_iter.count = 1;
+		msg->hdr.msg_iter.kvec = &msg->vec;
+		msg->hdr.msg_iter.nr_segs = 1;
+
+		old_fs = get_fs();
+		set_fs(KERNEL_DS);
+		size = sock_recvmsg(server_state->sock, &msg->hdr, 0);
+		set_fs(old_fs);
+
+		if (!server_state->running) {
+			goto out_buf;
+		}
+
+		if (size < 0) {
+			printk(KERN_WARNING "Failed to recieve message, skipping\n");
+			kfree(msg->vec.iov_base);
+			kfree(msg);
+			continue;
+		}
+
+		respond(msg);
 	}
 
-	return NF_ACCEPT;
+out_buf:
+	kfree(msg->vec.iov_base);
+out_msg:
+	kfree(msg);
+out:
+	return ret;
 }
-
-static const struct nf_hook_ops hook_ops = {
-	.hook		= packet_interceptor,
-	.pf		= NFPROTO_INET,
-	.hooknum	= NF_INET_LOCAL_IN,
-	.priority	= NF_IP_PRI_FIRST
-};
 
 static int init_kern_store(void)
 {
+	struct sockaddr_in bind_addr;
 	int ret = 0;
-	ret = nf_register_net_hook(&init_net, &hook_ops);
+	server_state = kzalloc(sizeof(struct kern_store), 0);
+	if (!server_state) {
+		printk(KERN_WARNING "Cannot allocate memory for server state\n");
+		ret = -ENOMEM;
+		goto out_err;
+	}
 
+	ret = sock_create_kern(&init_net, AF_INET, SOCK_DGRAM, IPPROTO_UDP, &server_state->sock);
+	if (ret < 0) {
+		printk(KERN_WARNING "Failed to create incoming socket\n");
+		goto out_free_state;
+	}
+
+	bind_addr.sin_family = AF_INET;
+	bind_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	bind_addr.sin_port = htons(CHAIN_PORT);
+	ret = server_state->sock->ops->bind(server_state->sock, (struct sockaddr*)&bind_addr, sizeof(struct sockaddr_in));
+	if (ret) {
+		printk(KERN_WARNING "Failed to bind receiving socket\n");
+		goto out_release;
+	}
+
+	server_state->running = 1;
+	server_state->thread = kthread_run(run_server, NULL, "kkvd");
+	if (IS_ERR(server_state->thread)) {
+		ret = -ENOMEM;
+		printk(KERN_WARNING "Failed to start kkvd thread.\n");
+		goto out_release;
+	}
+
+	return ret;
+
+out_release:
+	sock_release(server_state->sock);
+out_free_state:
+	kfree(server_state);
+out_err:
 	return ret;
 }
 
 static void remove_kern_store(void)
 {
-	nf_unregister_net_hook(&init_net, &hook_ops);
-}
+	if (server_state) {
+		server_state->running = 0;
+		if (server_state->thread) {
+			kthread_stop(server_state->thread);
+			msleep(10);
+			server_state->thread = NULL;
+		}
 
-#else
+		if (server_state->sock) {
+			sock_release(server_state->sock);
+			server_state->sock = NULL;
+		}
 
-static int init_kern_store(void)
-{
+		kfree(server_state);
+	}
 }
-
-static void remove_kern_store(void)
-{
-}
-#endif
 
 module_init(init_kern_store);
 module_exit(remove_kern_store);
